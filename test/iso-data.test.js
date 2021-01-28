85380c62e363f83eb8def3a53217cfb3f24d6ef9
Justin Hammond <39606064+Justintime50@users.noreply.github.com>
2021-01-27 23:59:35 -0700

    Adds support for ISO country codes (closes #7) (#10)

---
+++ test/iso-data.test.js
@@ -0,0 +1,18 @@
+/* eslint-env node, mocha */
+const assert = require('chai').assert
+const expect = require('chai').expect
+const dad = require('../index')
+
+describe('ISO Country Code Data', function () {
+    const isoData = dad.isoCountryCodes()
+
+    it('returns a list of ISO country codes with a length of 249', function () {
+        assert.equal(isoData.length, 249);
+    });
+
+    isoData.forEach(function (singleIsoCountry) {
+        it(`ensures the data structure of "${singleIsoCountry.country}" is uniform`, function () {
+            expect(singleIsoCountry).to.contain.all.keys('country', 'alpha_2_code', 'alpha_3_code');
+        });
+    })
+})
